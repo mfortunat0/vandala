847b4bf52deec14a7036ed8b8790890cf763286d
matheus <matheus.xmaz10@gmail.com>
2022-01-18 20:19:42 -0300

    add ormconfig

---
+++ ormconfig.js
@@ -0,0 +1,8 @@
+export default {
+  type: process.env.TYPEORM_TYPE,
+  host: process.env.TYPEORM_HOST,
+  port: process.env.TYPEORM_PORT,
+  username: process.env.TYPEORM_USERNAME,
+  password: process.env.TYPEORM_PASSWORD,
+  database: process.env.TYPEORM_DATABASE,
+};
