3de7006bfc4ec538bf2db4094e13bc9ab1b62acc
matheus <matheus.xmaz10@gmail.com>
2022-01-18 22:08:19 -0300

    add user controller

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from "express";
+import { container } from "tsyringe";
+import { UserService } from "../services/UserService";
+
+export class UserController {
+  async create(request: Request, response: Response) {
+    const { name } = request.body;
+    const userService = container.resolve(UserService);
+    const user = await userService.create({ name });
+    response.json(user);
+  }
+
+  async find(request: Request, response: Response) {
+    const userService = container.resolve(UserService);
+    const users = await userService.find();
+    response.json(users);
+  }
+}
