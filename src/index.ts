f9dd47b05a8085becc8cd2663c2aa8df5425d941
matheus <matheus.xmaz10@gmail.com>
2022-01-18 22:08:01 -0300

    add routes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
+import "dotenv/config";
+import "./config/database";
+import "./config/container";
 import express from "express";
+import { UserController } from "./controllers/UserController";
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+const userController = new UserController();
+app.get("/user", userController.find);
+app.post("/user", userController.create);
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
