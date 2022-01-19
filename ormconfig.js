73ed9d04251977862eba189ac071c1451291aecc
matheus <matheus.xmaz10@gmail.com>
2022-01-18 22:07:07 -0300

    add typeorm config

--- ormconfig.js
+++ ormconfig.js
@@ -1,8 +1,14 @@
-export default {
+module.exports = {
   type: process.env.TYPEORM_TYPE,
   host: process.env.TYPEORM_HOST,
   port: process.env.TYPEORM_PORT,
   username: process.env.TYPEORM_USERNAME,
   password: process.env.TYPEORM_PASSWORD,
   database: process.env.TYPEORM_DATABASE,
+  synchronize: true,
+  entities: ["./src/models/**/*.ts"],
+  migrations: ["./src/config/database/migrations/*.ts"],
+  cli: {
+    migrationsDir: "./src/config/database/migrations",
+  },
 };
