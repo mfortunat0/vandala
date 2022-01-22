0ebb07f55e3509feb847e0d3a249954b4a03df07
matheus <matheus.xmaz10@gmail.com>
2022-01-21 21:09:23 -0300

    add columns for created/updated

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,11 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  OneToMany,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from "typeorm";
 
 @Entity()
 export class User {
@@ -16,4 +23,10 @@ export class User {
 
   @Column()
   hashPassword: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
 }
