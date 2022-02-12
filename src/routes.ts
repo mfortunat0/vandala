b09e290605eacc89466086719bc6c99e03a33bc9
matheus <matheus.xmaz10@gmail.com>
2022-02-12 09:58:16 -0300

    add swagger docs

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import morgan from "morgan";
+import swaggerUi from "swagger-ui-express";
+import swaggerOptions from "./swagger.json";
 import { userRoutes } from "./routes/UserRoutes";
 
 const routes = Router();
 
 routes.use(morgan("common"));
 routes.use("/user", userRoutes);
+routes.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerOptions));
 routes.get("/check", (request, response) => response.send("OK"));
 
 export { routes };
--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -6,21 +6,16 @@ import { ensureAuthenticate } from "../middlewares/authMiddleware";
 const userRoutes = Router();
 const userController = new UserController();
 
-userRoutes.get("/:id", ensureAuthenticate, userController.findAll);
-userRoutes.get("", ensureAuthenticate, userController.findAll);
-userRoutes.post("", userController.create);
+userRoutes.get("/:id", ensureAuthenticate, userController.findById);
 userRoutes.put("/:id", ensureAuthenticate, userController.update);
-
 userRoutes.delete("/:id", ensureAuthenticate, userController.delete);
+
+userRoutes.get("", ensureAuthenticate, userController.findAll);
+userRoutes.post("", ensureAuthenticate, userController.create);
 
 userRoutes.post("/login", userController.login);
 userRoutes.post("/deposit/:id", ensureAuthenticate, userController.deposit);
 userRoutes.post("/withdraw/:id", ensureAuthenticate, userController.withdraw);
-userRoutes.post(
-  "/transfer/:idFrom/to/:idTo",
-  ensureAuthenticate,
-  userController.transfer
-);
 userRoutes.post(
   "/transfer/:idFrom/to/:idTo",
   ensureAuthenticate,
